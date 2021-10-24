4c2aa4e01236424040d61f3aec16c7f76b4f59e4
Etienne Tramaux <etienne.tramaux@gmail.com>
2021-10-24 14:30:04 +0200

    Added color to sprite files and corrected parser

--- src/AsciiSprite.cpp
+++ src/AsciiSprite.cpp
@@ -2,8 +2,35 @@
 
 #include <iostream>
 #include <fstream>
+#include <map>
 
 using namespace std;
+
+#define STR_TO_COLOR_WITH_PREFIX(prefix)                                \
+    std::map<std::string, AsciiSprite::Color> strToColor##prefix = {    \
+                                                                        \
+            {"red", AsciiSprite::prefix##_RED},                         \
+            {"blue", AsciiSprite::prefix##_BLUE},                       \
+            {"green", AsciiSprite::prefix##_GREEN},                     \
+                                                                        \
+            {"lightred", AsciiSprite::prefix##_LIGHTRED},               \
+            {"lightgreen", AsciiSprite::prefix##_LIGHTGREEN},           \
+            {"lightblue", AsciiSprite::prefix##_LIGHTBLUE},             \
+                                                                        \
+            {"yellow", AsciiSprite::prefix##_YELLOW},                   \
+            {"magenta", AsciiSprite::prefix##_MAGENTA},                 \
+            {"cyan", AsciiSprite::prefix##_CYAN},                       \
+                                                                        \
+            {"lightyellow", AsciiSprite::prefix##_LIGHTYELLOW},         \
+            {"lightmagenta", AsciiSprite::prefix##_LIGHTMAGENTA},       \
+            {"lightcyan", AsciiSprite::prefix##_LIGHTCYAN},             \
+                                                                        \
+            {"black", AsciiSprite::prefix##_BLACK},                     \
+            {"white", AsciiSprite::prefix##_WHITE},                     \
+    };                                                                  \
+
+STR_TO_COLOR_WITH_PREFIX(FG)
+STR_TO_COLOR_WITH_PREFIX(BG)
 
 AsciiSprite::AsciiSprite(const string& filename) {
 
@@ -15,22 +42,97 @@ AsciiSprite::AsciiSprite(const string& filename) {
 	//Initiate height and width of a sprite according to the first line
 	input >> width;
 	input >> height;
-	string* asciiTab = new string[height];
 	getline(input, nextLine);
 
-	int i = 0;
-	while (!getline(input, nextLine).fail()) {
-		asciiTab[i] = nextLine;
-		i++;
-	}
+    // Get the ascii data and fill the matrix
+    asciiArt = Matrix<CHAR_INFO>(height, width);
+    for (int i = 0; i < height; i++) {
 
-	asciiArt = Matrix<CHAR_INFO>(height, width);
-	for (int i = 0; i < height-1; i++) {
-		for (int j = 0; j < width; j++) {
-			asciiArt(i, j).Char.AsciiChar = asciiTab[i][j];
+        if(!input.good()) {
+            return;
+        }
+
+        getline(input, nextLine);
+
+        for (int j = 0; j < width; j++) {
+            CHAR c = j < nextLine.size() ? nextLine[j] : ' ';
+            asciiArt(i, j).Char.AsciiChar = c;
             asciiArt(i, j).Attributes = FG_WHITE | BG_BLACK;
-		}
-	}
+        }
+
+    }
+
+    if(!input.good()) {
+        return;
+    }
+
+    // The number of color
+    int colorCount = 0;
+    input >> colorCount;
+    getline(input, nextLine);
+
+    std::map<uint8_t, std::string> colors;
+
+    // Get each color
+    for(int i = 0; i < colorCount; ++i) {
+
+        int colorID;
+        std::string colorName;
+
+        input >> colorID;
+        input >> colorName;
+        colors[colorID] = colorName;
+
+        getline(input, nextLine);
+    }
+
+    // The empty line
+    getline(input, nextLine);
+
+    // Foreground
+    for (int i = 0; i < height; i++) {
+
+        if(!input.good()) {
+            return;
+        }
+
+        getline(input, nextLine);
+        for (int j = 0; j < nextLine.size(); j++) {
+
+            uint8_t colorID = nextLine[j] - '0';
+            const std::string& colorName = colors[colorID];
+            auto color = strToColorFG[colorName];
+
+            asciiArt(i, j).Attributes = color;
+
+        }
+
+
+    }
+
+    // The empty line
+    getline(input, nextLine);
+
+    // Background
+    for (int i = 0; i < height; i++) {
+
+        if(!input.good()) {
+            return;
+        }
+
+        getline(input, nextLine);
+        for (int j = 0; j < nextLine.size(); j++) {
+
+            uint8_t colorID = nextLine[j] - '0';
+            const std::string& colorName = colors[colorID];
+            auto color = strToColorBG[colorName];
+
+            asciiArt(i, j).Attributes |= color;
+
+        }
+
+    }
+
 
 }
 
